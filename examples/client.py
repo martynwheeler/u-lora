490fb34b19157c76ddffd10addd8727e28a8d2f9
martynwheeler <martyn.wheeler@physics.org>
2021-01-30 13:30:45 +0000

    Update client.py

--- examples/client.py
+++ examples/client.py
@@ -3,7 +3,8 @@ from ulora import LoRa, ModemConfig
 
 # Lora Parameters
 RFM95_RST = 27
-RFM95_CS = 0
+RFM95_SPIBUS = 0
+RFM95_CS = 5
 RFM95_INT = 28
 RF95_FREQ = 868.0
 RF95_POW = 20
@@ -11,7 +12,8 @@ CLIENT_ADDRESS = 1
 SERVER_ADDRESS = 2
 
 # initialise radio
-lora = LoRa(RFM95_CS, RFM95_INT, CLIENT_ADDRESS, reset_pin=RFM95_RST, freq=RF95_FREQ, tx_power=RF95_POW, acks=True)
+lora = LoRa(RFM95_SPIBUS, RFM95_INT, CLIENT_ADDRESS, RFM95_CS, reset_pin=RFM95_RST, freq=RF95_FREQ, tx_power=RF95_POW, acks=True)
+
 
 # loop and send data
 while True:
